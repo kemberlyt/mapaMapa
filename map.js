65007c138589df4798b72969dbfd33eee58346d8
kemberlyt <kturcios@tywls.org>
2018-08-07 15:42:25 -0400

    fixes

--- map.js
+++ map.js
@@ -33,3 +33,43 @@ document.addEventListener("DOMContentLoaded", function (e){
     init();
     
     });
+
+function panHome(){
+    view.animate({
+        center: ourLoc,
+        duration: 2000
+    });
+} 
+
+    
+
+
+
+function panToLocation(){
+    var countryName = document.getElementById("country-name").value;
+
+    if(countryName ===""){
+    alert("You didn't enter a country name!");
+    return;
+    }
+    var lon = 0.0;
+    var lat = 0.0;
+    var location = ol.proj.fromLonLat([lon,lat]);
+
+    //
+    var query = "https://restcountries.eu/rest/v2/name/"+countryName;
+    query = query.replace(/ /g, "%20")
+    alert(query);
+    //
+    var countryRequest = new XMLHttpRequest();
+    countryRequest.open('GET', query, false);
+
+    countryRequest.send();
+
+
+    view.animate({
+        center:location,
+        duration:2000
+    });
+}
+
